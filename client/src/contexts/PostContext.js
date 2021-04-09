07e0cec3ee2e50408dcc7b08312f0749e0935207
“pntech20” <minhtoan611997@gmail.com>
2021-04-09 17:12:23 +0700

    hello

--- client/src/contexts/PostContext.js
+++ client/src/contexts/PostContext.js
@@ -19,7 +19,7 @@ const PostContextProvider = ({ children }) => {
     message: "",
     type: null,
   });
-  //get all posts
+  //get all postsasdas
   const getPosts = async () => {
     try {
       const response = await axios.get(`${apiUrl}/posts`);
